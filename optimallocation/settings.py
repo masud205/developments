4aafd7d9d1e122199e4ecbf52a86a628bf5266c0
masud205 <smmasudr@gmail.com>
2021-09-14 11:27:29 +0200

    Changes in DATABASES in setting.py

--- optimallocation/settings.py
+++ optimallocation/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'optimallocation.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'duproject',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
+
 
 
 # Password validation
