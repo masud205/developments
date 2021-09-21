b2097210a66ded3fed8591e92b24cc5658a2f926
masud205 <smmasudr@gmail.com>
2021-09-21 09:11:17 +0200

    Changes in views.py and storeLocations.html

--- optimallocation/settings.py
+++ optimallocation/settings.py
@@ -92,7 +92,8 @@ WSGI_APPLICATION = 'optimallocation.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        #'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': 'duproject',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
--- storeLocationsApp/views.py
+++ storeLocationsApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from storeLocationsApp.models import retailers, regions, municipalities, stores
+from django.core import serializers
+
 
 # Create your views here.
 
@@ -12,16 +14,25 @@ def get_dropdown_data(request):
     if request.method == "POST":
         retail_id = request.POST.get('retailers')
         reg_id = request.POST.get('regions')
-        
-        #ret_id = retailers.objects.get(retailer_name=retail_name)
-        #regid = regions.objects.get(region_name=reg_val)
-        municipal = municipalities.objects.filter(region_id=reg_id)
-        store = stores.objects.filter(retailer_id = retail_id, municipality_id__in=municipal.values('municipality_id'))
+        municipal_id = request.POST.get('municipalities')
+                
+        if reg_id == "---Select Region---" and municipal_id == "---Select Municipality---":
+            store = stores.objects.filter(retailer_id = retail_id, municipality_id__in=municipalobj.values('municipality_id')) 
+
+        elif reg_id != "---Select Region---" and municipal_id == "---Select Municipality---":
+            municipal = municipalities.objects.filter(region_id=reg_id)
+            store = stores.objects.filter(retailer_id = retail_id, municipality_id__in=municipal.values('municipality_id'))
+
+        else:
+            store = stores.objects.filter(retailer_id = retail_id, municipality_id = municipal_id)
+            store_data = serializers.serialize("json", store)
+            print(store_data)
+
         return render(request, 'storeLocations.html', {"retailers": retailerobj, "regions": regionobj,
-                                                   "municipalities": municipalobj, "find_stores": store});
+                                                   "municipalities": municipalobj, "find_stores": store_data});
     else:
         store = stores.objects.all()
         return render(request, 'storeLocations.html', {"retailers": retailerobj, "regions": regionobj,
-                                                   "municipalities": municipalobj, "find_storess": store});
+                                                   "municipalities": municipalobj, "find_all_stores": store});
 
 
