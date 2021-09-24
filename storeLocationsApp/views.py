d1418bcc8c2a7c0f0b53df82033a28b2ef19bd24
masud205 <smmasudr@gmail.com>
2021-09-24 14:56:24 +0200

    Showing search criteria

--- storeLocationsApp/views.py
+++ storeLocationsApp/views.py
@@ -17,28 +17,47 @@ def get_dropdown_data(request):
         retail_id = request.POST.get('retailers')
         reg_id = request.POST.get('regions')
         municipal_id = request.POST.get('municipalities')
+        
+        #get selected retailer information
+        try:
+            retailer = retailers.objects.get(retailer_id=retail_id)
+        except ValueError:
+            retailer = {'retailer_name':'No retailer selected'}
+
+        #get selected region information
+        try:
+            region = regions.objects.get(region_id=reg_id)
+        except ValueError:
+            region = {'region_name':'All regions'}
+        
+        #get selected municipality information
+        try:
+            municipality = municipalities.objects.get(municipality_id=municipal_id)
+        except ValueError:
+            municipality = {'municipality_name':'All municipalities'}
                 
         if reg_id == "---Select Region---" and municipal_id == "---Select Municipality---":
             store = stores.objects.filter(retailer_id = retail_id, municipality_id__in=municipalobj.values('municipality_id')) 
+            store_data = serializers.serialize("json", store)
 
         elif reg_id != "---Select Region---" and municipal_id == "---Select Municipality---":
             municipal = municipalities.objects.filter(region_id=reg_id)
             store = stores.objects.filter(retailer_id = retail_id, municipality_id__in=municipal.values('municipality_id'))
+            store_data = serializers.serialize("json", store)
 
         else:
             store = stores.objects.filter(retailer_id = retail_id, municipality_id = municipal_id)
             store_data = serializers.serialize("json", store)
             #store_js = simplejson.loads(store_data)
             #print(store_js)
-            print(store_data)
+            #print(store_data)
             #print(store)
             
             #print(store_js[0]['fields']['store_address'])
 
-        return render(request, 'storeLocations.html', {"retailers": retailerobj, "regions": regionobj,
+        return render(request, 'storeLocations.html', {"selected_retailer": retailer, "selected_region": region, "selected_municipal": municipality, "retailers": retailerobj, "regions": regionobj,
                                                    "municipalities": municipalobj, "find_stores": store_data});
      else:
-        print("Hello")
         store = stores.objects.all()
         return render(request, 'storeLocations.html', {"retailers": retailerobj, "regions": regionobj,
                                                    "municipalities": municipalobj, "find_stores": store});
